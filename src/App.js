ad634979c21291720360e3636b63913340d2dca9
Mateusz <mateusz@searchtalent.com>
2020-01-22 13:41:55 +0100

    added line and item containers

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import "./App.css";
 function App() {
   return (
     <div id="flex-container">
-      <div id="header">Header</div>
-      <div id="body">Body</div>
+      <div id="header">
+        <div id="logo">Logo</div>
+        <div id="game_info">Game info</div>
+        <div id="lang_settings">Lang Settings</div>
+      </div>
+
+      <div id="body">
+        <div id="item_container">
+          <div className="item">Item 1</div>
+          <div className="item">Item 2</div>
+        </div>
+
+        <div id="line_container">
+          line container
+          <div className="line_spot">line spot</div>
+        </div>
+      </div>
+
       <div id="footer">Footer</div>
     </div>
   );
