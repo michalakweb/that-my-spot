16cce0a55669ea61a9355f3170b328f97dff17d0
Mateusz <mateusz@searchtalent.com>
2020-02-03 13:52:31 +0100

    fixed card error

--- src/components/footer/CardContainer.js
+++ src/components/footer/CardContainer.js
@@ -4,7 +4,11 @@ import Card from "./Card";
 const num = 5;
 
 const CardContainer = () => (
-  <div id="card_container">{[...Array(num).map(() => <Card />)]}</div>
+  <div id="card_container">
+    {[...Array(num)].map(() => (
+      <Card />
+    ))}
+  </div>
 );
 
 export default CardContainer;
