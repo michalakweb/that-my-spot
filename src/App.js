7802e7313f4fd03dcf42c7c196b11ab5c13731d7
Mateusz <mateusz@searchtalent.com>
2020-02-10 16:35:22 +0100

    state variables added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,56 @@ import Header from "./components/header/Header";
 import Body from "./components/body/Body";
 import Footer from "./components/footer/Footer";
 
-function App() {
-  return (
-    <div id="container">
-      <div id="flex-container">
-        <Header />
+// Deck & Items db
+const deckStats = [{ id: 1 }, { id: 2 }];
+// const itemStats = [{ id: 1 }, { id: 2 }];
 
-        <Body />
+class App extends React.Component {
+  state = {
+    // Game info
+    deckPlayer: JSON.parse(JSON.stringify(deckStats)),
+    deckComputer: JSON.parse(JSON.stringify(deckStats)),
+    currentItems: [],
+    playerItems: 0,
+    computerItems: 0,
+    lineCards: [],
+    drawnCardPlayer: null,
+    drawnCardComputer: null,
+    chosenCard: null,
+    lineStats: null,
 
-        <Footer />
+    // Phase tracking
+    currentPhase: 0,
+    phaseCounter: 0,
+
+    // Scores
+    playerScore: 0,
+    computerScore: 0,
+    playerOverallScore: 0,
+    computerOverallScore: 0,
+
+    // Settings
+    currentLanguage: "en",
+
+    // Modals
+    showWelcomeScreen: false,
+    showWinLoseScreen: false,
+    showItemsScreen: false
+  };
+
+  render() {
+    return (
+      <div id="container">
+        <div id="flex-container">
+          <Header />
+
+          <Body />
+
+          <Footer />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
