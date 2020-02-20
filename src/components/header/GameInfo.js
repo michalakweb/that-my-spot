a56963e9a68a44bd5c731febaba4a59adc136dd2
Mateusz <mateusz@searchtalent.com>
2020-02-20 11:23:55 +0100

    game info change

--- src/components/header/GameInfo.js
+++ src/components/header/GameInfo.js
@@ -4,7 +4,7 @@ const GameInfo = props => (
   <div id="game_info">
     <p>turn counter: {props.turnCounter}</p>
     <p>player score: {props.playerScore}</p>
-    <p>player score: {props.computerScore}</p>
+    <p>ai score: {props.computerScore}</p>
   </div>
 )
 
