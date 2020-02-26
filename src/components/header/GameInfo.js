53a1186dc62502b9fbb14b40f1d01df37ed95bc3
Mateusz <mateusz@searchtalent.com>
2020-02-26 13:21:52 +0100

    added simple computer card

--- src/components/header/GameInfo.js
+++ src/components/header/GameInfo.js
@@ -1,10 +1,22 @@
 import React from "react"
+import computerImage from "../../images/odra.jpg"
 
 const GameInfo = props => (
   <div id="game_info">
-    <p>turn counter: {props.turnCounter}</p>
-    <p>player score: {props.playerScore}</p>
-    <p>ai score: {props.computerScore}</p>
+    <div id="player_info_container">
+      <p>turn left: {3 - props.turnCounter / 2}</p>
+      <p>player score: {props.playerScore}</p>
+    </div>
+    <div id="computer_info_container">
+      <div id="computer_card">
+        <div id="computer_card_photo">
+          <img src={computerImage} alt="old computer" />
+        </div>
+        <div id="computer_card_info">
+          <p>ai score: {props.computerScore}</p>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
