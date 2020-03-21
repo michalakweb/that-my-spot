1c305dab19f28116b9a68708086381ef5fd9a606
Mateusz <michalakweb@gmail.com>
2020-03-21 16:24:27 +0100

    small fixes

--- src/components/header/GameInfo.js
+++ src/components/header/GameInfo.js
@@ -3,36 +3,36 @@ import computerImage from "../../images/odra.jpg"
 import playerImage from "../../images/player.jpg"
 
 const GameInfo = props => (
-  <div id="game_info">
-    <div id="player_info_container">
-      <div id="card">
-        <div id="card_photo">
-          <img
-            src={playerImage}
-            alt="face of communist pioneer"
-            className={!props.computerThinking && "pulsePlayer"}
-          />
-        </div>
-        <div id="card_info">
-          <p>{props.playerScore}</p>
-        </div>
-      </div>
-    </div>
-    <div id="computer_info_container">
-      <div id="card">
-        <div id="card_photo">
-          <img
-            src={computerImage}
-            alt="old computer"
-            className={props.computerThinking && "pulse"}
-          />
-        </div>
-        <div id="card_info">
-          <p>{props.computerScore}</p>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div id="game_info">
+		<div id="player_info_container">
+			<div id="card">
+				<div id="card_photo">
+					<img
+						src={playerImage}
+						alt="face of communist pioneer"
+						className={!props.computerThinking ? "pulsePlayer" : "undefined"}
+					/>
+				</div>
+				<div id="card_info">
+					<p>{props.playerScore}</p>
+				</div>
+			</div>
+		</div>
+		<div id="computer_info_container">
+			<div id="card">
+				<div id="card_photo">
+					<img
+						src={computerImage}
+						alt="old computer"
+						className={props.computerThinking ? "pulse" : "undefined"}
+					/>
+				</div>
+				<div id="card_info">
+					<p>{props.computerScore}</p>
+				</div>
+			</div>
+		</div>
+	</div>
 )
 
 export default GameInfo
