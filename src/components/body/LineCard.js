97b0933616d9f69e187fff746e3ec32c600c9620
Mateusz <michalakweb@gmail.com>
2020-03-21 16:21:02 +0100

    introduced first card image in deck and line

--- src/components/body/LineCard.js
+++ src/components/body/LineCard.js
@@ -1,10 +1,15 @@
 import React from "react"
 
 const LineCard = props => (
-  <div className="line_card">
-    {props.lineCard.name}
-    {props.lineCard.value}
-  </div>
+	<div className="line_card">
+		{/* {props.lineCard.name}
+		{props.lineCard.value} */}
+		<img
+			alt="nurse card"
+			className="cardPhoto"
+			src={`/cards/${props.lineCard.id}${props.lineCard.name}.png`}
+		/>
+	</div>
 )
 
 export default LineCard
--- src/components/footer/Card.js
+++ src/components/footer/Card.js
@@ -1,17 +1,24 @@
 import React from "react"
 
 const Card = props => (
-  <div
-    className={`card card${props.stylePosition}`}
-    onClick={() => {
-      props.selectCard(props.cardInfo, props.position)
-    }}
-  >
-    <div className="card_header">
-      <p>{props.cardInfo.value}</p>
-    </div>
-    <div className="card_body">{props.cardInfo.name}</div>
-  </div>
+	<div
+		className={`card card${props.stylePosition}`}
+		onClick={() => {
+			props.selectCard(props.cardInfo, props.position)
+		}}
+	>
+		<div className="card_header">
+			<p>{props.cardInfo.value}</p>
+		</div>
+		<div className="card_body">
+			{/* {props.cardInfo.name} */}
+			<img
+				alt={props.cardInfo.name}
+				className="deckCardPhoto"
+				src={`/cards/${props.cardInfo.id}${props.cardInfo.name}.png`}
+			/>
+		</div>
+	</div>
 )
 
 export default Card
