fe736bb58cd2519de0379a1c251ebe93486e2f08
Mateusz <michalakweb@gmail.com>
2020-04-28 16:17:04 +0200

    styled win-lose screen a bit

--- src/components/WinLoseScreen.js
+++ src/components/WinLoseScreen.js
@@ -1,16 +1,17 @@
 import React from "react"
 
 const WinLoseScreen = (props) => (
-	<div id="welcome_screen">
-		<div id="welcome_logo">
+	<div id="win_lose_screen">
+		<div id="win_lose_info_container">
 			{props.playerFinalScore > props.computerFinalScore ? (
 				<p>You won!</p>
 			) : (
-				<p>The AI won! You lost!</p>
+				<p>You lost!</p>
 			)}
 		</div>
 		<div id="button_container">
 			<button
+				id="start_game_btn"
 				onClick={() => {
 					props.switchWinLoseScreen()
 				}}
