f15f086678dbc97b5cdc0d8121dee26b013045ee
Mateusz <michalakweb@gmail.com>
2020-04-28 14:33:53 +0200

    welcome screen styled for now

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,16 +1,8 @@
 import React from "react"
 import logo from "../images/logo.png"
-import filmRoll1 from "../images/filmRoll1.png"
-import lenin from "../images/lenin.jpg"
 
 const WelcomeScreen = (props) => (
 	<div id="welcome_screen">
-		<div id="film_roll_1">
-			<img src={filmRoll1}></img>
-		</div>
-		<div id="lenin">
-			<img src={lenin}></img>
-		</div>
 		<div id="welcome_logo">
 			<img src={logo} alt="game logo" />
 		</div>
