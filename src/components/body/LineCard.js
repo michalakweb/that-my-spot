b9f4a72d0e1aee9d95232ff93d27afd0d751b71c
Mateusz <michalakweb@gmail.com>
2020-05-01 16:57:21 +0200

    smoker stops smoking when nurse nearby

--- src/components/body/LineCard.js
+++ src/components/body/LineCard.js
@@ -2,20 +2,22 @@ import React from "react"
 
 const LineCard = (props) => (
 	<div className="line_card">
-		{props.lineCard.name === "palacz" && (
-			<div id="smoke">
-				<span className="s0"></span>
-				<span className="s1"></span>
-				<span className="s2"></span>
-				<span className="s3"></span>
-				<span className="s4"></span>
-				<span className="s5"></span>
-				<span className="s6"></span>
-				<span className="s7"></span>
-				<span className="s8"></span>
-				<span className="s9"></span>
-			</div>
-		)}
+		{props.lineCard.name === "palacz" &&
+			!props.lineCard.effect &&
+			props.lineCard.effect !== "scared" && (
+				<div id="smoke">
+					<span className="s0"></span>
+					<span className="s1"></span>
+					<span className="s2"></span>
+					<span className="s3"></span>
+					<span className="s4"></span>
+					<span className="s5"></span>
+					<span className="s6"></span>
+					<span className="s7"></span>
+					<span className="s8"></span>
+					<span className="s9"></span>
+				</div>
+			)}
 		<img
 			alt="nurse card"
 			className="cardPhoto"
