37d5882d50472eb83db10ff3aaf663ea3ad976bd
Mateusz <michalakweb@gmail.com>
2020-05-11 21:03:55 +0200

    smoke effect spans made programatically

--- src/components/body/LineCard.js
+++ src/components/body/LineCard.js
@@ -15,16 +15,11 @@ const LineCard = props => (
 			!props.lineCard.effect &&
 			props.lineCard.effect !== "scared" && (
 				<div id="smoke">
-					<span className="s0"></span>
-					<span className="s1"></span>
-					<span className="s2"></span>
-					<span className="s3"></span>
-					<span className="s4"></span>
-					<span className="s5"></span>
-					<span className="s6"></span>
-					<span className="s7"></span>
-					<span className="s8"></span>
-					<span className="s9"></span>
+					{Array(10)
+						.fill(0)
+						.map((el, id) => (
+							<span key={id} className={`s${id}`}></span>
+						))}
 				</div>
 			)}
 		{props.lineCard.name === "pielegniarka" &&
