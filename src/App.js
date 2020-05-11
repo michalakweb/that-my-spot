35b17187374ec5b38b404f44a2a3d80c12b77bc0
Mateusz <michalakweb@gmail.com>
2020-05-11 20:06:31 +0200

    items current sorted outside of set State

--- src/App.js
+++ src/App.js
@@ -10,158 +10,8 @@ import WinLoseScreen from "./components/WinLoseScreen"
 import SmallWidthScreen from "./components/SmallWidthScreen"
 import TutorialScreen from "./components/TutorialScreen"
 
-// Deck & Items db
-const deckStats = [
-	{ id: 1, name: "pielegniarka", value: 1 },
-	{ id: 2, name: "baba", value: 2 },
-	{ id: 3, name: "dzieciak", value: 1 },
-	{ id: 4, name: "palacz", value: 1 },
-	{ id: 5, name: "partyjniak", value: 4 }
-]
-const deckStats2 = [
-	{ id: 2, name: "baba", value: 2 },
-	{ id: 2, name: "baba", value: 2 },
-	{ id: 3, name: "dzieciak", value: 1 },
-	{ id: 4, name: "palacz", value: 1 },
-	{ id: 5, name: "partyjniak", value: 4 }
-]
-const deckStats3 = [
-	{ id: 1, name: "pielegniarka", value: 1 },
-	{ id: 2, name: "baba", value: 2 },
-	{ id: 4, name: "palacz", value: 1 },
-	{ id: 2, name: "baba", value: 2 },
-	{ id: 5, name: "partyjniak", value: 4 }
-]
-const itemStats = [
-	{
-		id: 1,
-		name: "magnetofon",
-		value: 5,
-		trueName: "Tape recorder AKAI 4000DS",
-		description:
-			"This reel-to-reel tape recorder was made by Akai, a Japanese company based in Tokio, now defunct. Produced in 1972-1973, it was considered a luxury product (almost everything produced outside of the Soviet Union was considered that).\n \n Tape recorders were extremely popular in the Eastern Block. Poland at the time didn't have extensive copyright laws and many radio stations played western albums in their entirety, so people could record them with tape recorders. Availability of western music was scarce and the prices too high for an average citizen."
-	},
-	{
-		id: 2,
-		name: "kotek",
-		value: 1,
-		trueName: "Cheap plastic brooch",
-		description:
-			"A brooch made of hideous plastic with a typical fastener, a safety pin. Product from the eighties. "
-	},
-	{
-		id: 3,
-		name: "komputer",
-		value: 5,
-		trueName: "ELWRO 800 JUNIOR",
-		description:
-			"A microcomputer produced in the eightes for schools. Its heart was the popular at the time 3.5MHz processor Z80 (Zilog). The computer was equipped with 'Basic' programming language interpreter. In one of its modes it was compatible with ZX Spectrum - an undeniable advantage.\n \n For its production the case from 'Elwirka' electronic keyboard was used (both made by the same company)."
-	},
-	{
-		id: 4,
-		name: "klawisze",
-		value: 5,
-		trueName: "Electronic keyboard 'Elwirka'",
-		description:
-			"'Elwirka' was produced in the eighties and it was meant for kids and teenagers. The quality of the keyboard was pretty good - 2.5octaves, polifonic system and one timbre that could be modified with functions, like vibrato.\n \n Modifications could be mixed, which gave even more sound editing options. A built-in amplifier produced a clear, loud sound, but if that wasn't enough, the user could connect the keyboard with external speakers."
-	},
-	{
-		id: 5,
-		name: "auto",
-		value: 5,
-		trueName: "Fiat 126P",
-		description:
-			"Constructed by Fiat and produced in Italy from 1973 and in Poland between 1973-2000. In Italy the car was marketed as the second, cheaper family car. In the much harsher economical reality of Poland, it was the only car families could aspire to.\n \n The car's diminutive dimensions inspired the nickname 'Maluch', which means 'toddler' or 'little one' in Polish."
-	},
-	{
-		id: 6,
-		name: "gra",
-		value: 3,
-		trueName: "Football family game",
-		description:
-			"An arcade game for at least two players simulating soccer.\n \n The rules of the game are simple. On the pitch there are figurines of players from both teams and two goals. Figures are mounted on springs, with which players can bounce the ball in any direction. In this way, you can pass the ball to other players or aim at the opponent's goal.\n \n Around the figures there are depressions thanks to which the ball can slide under the player's legs.\n \n The game provided great entertainment and was a real hit in socialist Poland."
-	},
-	{
-		id: 7,
-		name: "gumka",
-		value: 1,
-		trueName: "'Mouse' pencil eraser",
-		description:
-			"'Mouse' eraser - the iconic eraser was produced in several versions. Next to the pen, pencil and sharpener, it was in every school pencil case. In later years, the Mice were displaced for some time by fragrant Chinese erasers."
-	},
-	{
-		id: 8,
-		name: "herba",
-		value: 3,
-		trueName: "Indian tea 'Darjeeling'",
-		description:
-			"Loose black tea from the province of Darjeeling (India) or the surrounding area.\n \n Considered one of the best Indian teas. In this packaging, available in the eighties."
-	},
-	{
-		id: 9,
-		name: "herbata",
-		value: 2,
-		trueName: "'Popular' tea",
-		description:
-			"In the eighties it was actually one of the most popular teas.\n \n There wasn't much choice in the stores, so buying such tea was the only way. People just had to accept the reality (and product quality) of those times."
-	},
-	{
-		id: 10,
-		name: "rzutnik",
-		value: 4,
-		trueName: "Diascope Jota B-6",
-		description:
-			"A popular projector for displaying still black and white or color images from slides or positive film. Films were rolled and stored in small cardboard or plastic boxes.\n \n Due to the availability of a large number of fairy tales on film, this type of device was very much appreciated by children.\n \n The projector was produced at the turn of the sixties and seventies."
-	},
-	{
-		id: 11,
-		name: "papier",
-		value: 3,
-		trueName: "Stock of toilet paper",
-		description:
-			"Toilet paper was always lacking in Poland during the socialist period. This product was a luxury good for which people stuck in kilometer long queues.\n \n Poles have suffered for decades because of the constant lack of it. There have been cases that some have made desperate attempts to make toilet paper themselves by repeatedly crushing old newspapers."
-	},
-	{
-		id: 12,
-		name: "fajki",
-		value: 1,
-		trueName: "'Sport' cigarettes pack",
-		description:
-			"'Sport' cigarettes in the seventies were the cheapest generally available cigarettes (without filter).\n \n They could be purchased at every kiosk. At the turn of the seventies and eighties they were replaced by 'Popular' cigarettes. The name change was necessary because smoking with sport had little in common. (duh)"
-	},
-	{
-		id: 13,
-		name: "telewizorek",
-		value: 3,
-		trueName: "Mini-TV with slides",
-		description:
-			"Toy with microfilm containing several slides. To view the content you had to point the TV towards the light and then look with one eye in the viewfinder. Moving the slides consisted of rotating the protruding microfilm film with your finger.\n \n What's most interesting, these only a dozen or so slides managed to put the whole movie with characters from fairy tales. Product from the seventies and eighties of the last century."
-	},
-	{
-		id: 14,
-		name: "sokowirowka",
-		value: 4,
-		trueName: "Juicer 'Katarzyna'",
-		description:
-			"Food processor serving mainly as a juicer, but also a blender and grinder. A practical and very functional device, it took up little space.\n \n The first quality mark was in this case a reflection of a really solid, reliable and well thought-out construction."
-	},
-	{
-		id: 15,
-		name: "aparat",
-		value: 4,
-		trueName: "Camera ami 66",
-		description:
-			"A camera meant for amateur photogrpahy. Produced in the sixties and very fashionable at the time, especially during travel."
-	},
-	{
-		id: 16,
-		name: "colorofon",
-		value: 4,
-		trueName: "Colorofon C23-B",
-		description:
-			"Device for presenting light effects flashing to the rhythm of the music, produced in the eighties.\n \n This is an improved version of the C-23 model with lower power consumption. In a wooden case covered with veneer, three spotlights with colored filters are installed.\n \n To improve the aesthetics, the front panel is made of 'scratched' aluminum. The built-in electronic control system is designed to turn on the lights when specific sounds occur.\n \n Colorofon was used at home parties as well as class discos."
-	}
-]
+// cards and items
+import { deckStats, deckStats2, deckStats3, itemStats } from "./cardsAndItems"
 
 class App extends React.Component {
 	state = {
@@ -269,7 +119,7 @@ class App extends React.Component {
 					error: true
 				},
 				() => {
-					console.log("EMERGENCY", this.state)
+					console.error("EMERGENCY", this.state)
 				}
 			)
 		}
@@ -449,11 +299,13 @@ class App extends React.Component {
 	drawItemsFromDeck = array => {
 		console.log("drawing items")
 
-		var itemsCurrent = array.splice(-2, 2)
+		let itemsCurrent = array.splice(-2, 2)
+		itemsCurrent.sort((a, b) => b.value - a.value)
+
 		this.setState(
 			{
 				items: array,
-				itemsCurrent: itemsCurrent.sort((a, b) => b.value - a.value)
+				itemsCurrent: itemsCurrent
 			},
 			() => {
 				this.setState({
